95ea01be50afaa33297e2d985f19297db861b148
Egor I <egor.ivanov@pm.me>
2026-08-04 09:16:48 +0200

    up
    Signed-off-by: Egor I <egor.ivanov@pm.me>

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
-#!/bin/sh
-rm -rf ./allure-results
-npm install
-TESTS_SUCCESS=always
-TARGET=EGOR
 
+
+
+rm -rf ./allure-results
+pnpm install
+TESTS_SUCCESS=always
+TARGET=DEMO
+
+export ALLURE_PROJECT_ID=5030
 export ALLURE_TOKEN=$(security find-generic-password -a "$USER" -s "${TARGET}_ALLURE_TOKEN" -w)
 export ALLURE_ENDPOINT=$(security find-generic-password -a "$USER" -s "${TARGET}_ALLURE_ENDPOINT" -w)
 export ALLURE_RESULTS="allure-results"
-export ALLURE_PROJECT_ID=1
 export ALLURE_LAUNCH_NAME="$(date "+%Y-%m-%d %H%M%S") allurectl local upload target: ${TARGET} ${ALLURE_RESULTS} Prj: ${ALLURE_PROJECT_ID} "
 export ALLURE_LAUNCH_TAGS="upload, full-test"
-export TESTS_BROWSER="lalal"
+export TESTS_BROWSER="chrome"
 
-allurectl watch -- npm run test test/attachments/
+allurectl watch -- pnpm exec playwright test
