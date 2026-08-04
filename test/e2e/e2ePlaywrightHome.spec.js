25c6b113173963f75e652736b9bef3f2d90ab16e
Egor I <egor.ivanov@pm.me>
2026-08-04 12:10:55 +0200

    adds metadata
    Signed-off-by: Egor I <egor.ivanov@pm.me>

--- test/e2e/e2ePlaywrightHome.spec.js
+++ test/e2e/e2ePlaywrightHome.spec.js
@@ -1,7 +1,23 @@
 const { expect, test } = require("@playwright/test");
 const allure = require("allure-js-commons");
 
+test.beforeEach(async () => {
+  console.log("beforeEach");
+  await allure.attachment("One small txt attachment", "Welcome to wonderland!", "text/plain");
+});
+
+test.afterEach(async () => {
+  console.log("afterEach");
+  await allure.attachment("after each test we attach text", "That's all folks!", "text/plain");
+});
+
+
 test("Playwright homepage contains 'Playwright CLI'", async ({ page }) => {
+  await allure.tags("web", "playwright", "smoke");
+  await allure.epic("Allure Framework");
+  await allure.feature("Integration with Playwright");
+  await allure.story("Demo page test");
+  await allure.layer("e2e");
   await page.goto("https://playwright.dev");
   await expect(page.getByText("Playwright CLI")).toBeVisible();
 });
