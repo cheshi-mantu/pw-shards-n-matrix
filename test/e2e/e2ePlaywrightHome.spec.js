b1092a354fcf137c3a1155a7a52f401a5fff7eec
Egor I <egor.ivanov@pm.me>
2026-08-04 17:20:22 +0200

    rm imports
    Signed-off-by: Egor I <egor.ivanov@pm.me>

--- test/e2e/e2ePlaywrightHome.spec.js
+++ test/e2e/e2ePlaywrightHome.spec.js
@@ -1,5 +1,4 @@
 const { expect, test } = require("@playwright/test");
-const allure = require("allure-js-commons");
 
 test("Playwright homepage contains 'Playwright CLI'", async ({ page }) => {
   await page.goto("https://playwright.dev");
