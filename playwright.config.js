19ac27da9f7c578fa56f64af7078a31fab62168f
Egor I <egor.ivanov@pm.me>
2026-08-04 11:48:35 +0200

    clean pw
    Signed-off-by: Egor I <egor.ivanov@pm.me>

--- playwright.config.js
+++ playwright.config.js
@@ -4,12 +4,6 @@ export default defineConfig({
   testDir: "./test",
   reporter: [
     ["list"],
-    ['html', { outputFolder: './playwright-report', open: 'never' }],
-    ['allure-playwright', {
-      resultsDir: "./allure-results",
-      environmentInfo: {
-        node_version: process.version,
-      },
-    }],
+    ['html', { outputFolder: './playwright-report', open: 'never' }]
   ],
 });
